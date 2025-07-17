0192098d307b4cba58c837ef80afa16536338db3
aminjafarlou <jafarlou.amin@gmail.com>
2025-07-17 14:01:52 +0330

    improve UI

--- src/app/components/ResumeSection.tsx
+++ src/app/components/ResumeSection.tsx
@@ -4,7 +4,7 @@ import { renderItems } from './renderItems'
 export const ResumeSection = () => {
   return sections.map((section, index) => (
     <div key={index} className="space-y-4 mt-8">
-      <div className="bg-blue-100 text-center text-lg font-semibold py-2 rounded">{section.title}</div>
+      <div className="bg-sky-800 text-sky-50 text-center text-lg font-semibold py-2 rounded">{section.title}</div>
       {renderItems(section.items)}
     </div>
   ))
--- src/app/components/renderItems.tsx
+++ src/app/components/renderItems.tsx
@@ -5,18 +5,18 @@ export interface Item {
 
 export const renderItems = (items: Item[], level: number = 0) => {
   const fontSize = ['text-base', 'text-sm', 'text-xs', 'text-[10px]']
-  const textColor = ['text-gray-900', 'text-gray-700', 'text-gray-500', 'text-gray-400']
+  const textColor = ['text-gray-950', 'text-gray-900', 'text-gray-800', 'text-gray-700']
   const padding = `pl-${level * 4}`
 
   return (
     <ul
       className={`list-disc px-8 mb-2 space-y-1 ${padding} ${fontSize[level] || 'text-[10px]'} ${
-        textColor[level] || 'text-gray-400'
+        textColor[level] || 'text-'
       }`}
     >
       {items.map((item, index) => (
         <li key={index}>
-          <div className={level === 0  ? `font-bold` : ''}>{item.title}</div>
+          <div className={level === 0 ? `font-bold` : ''}>{item.title}</div>
           {item.subItems && renderItems(item.subItems, level + 1)}
         </li>
       ))}
