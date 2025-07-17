2774b9469135f668162a5e77b44278d324b0b410
aminjafarlou <jafarlou.amin@gmail.com>
2025-07-17 17:49:37 +0330

    Locally import the dynamic imports

--- src/app/components/AppHeader.tsx
+++ src/app/components/AppHeader.tsx
@@ -1,30 +1,23 @@
-import dynamic from 'next/dynamic'
-import { contactInfo } from '../data/information'
-import { PagePdf } from './pdf/PagePdf'
+import dynamic from 'next/dynamic';
+import { contactInfo } from '../data/information';
+import { PagePdf } from './pdf/PagePdf';
 
-const PDFDownloadLink = dynamic(
-  async () => {
-    const mod = await import('@react-pdf/renderer')
-    return mod.PDFDownloadLink
-  },
-  {
-    ssr: false,
-    loading: () => <p>Loading...</p>,
-  }
-)
+const PDFViewer = dynamic(() => import("./pdf/pdfViewer"), {
+  ssr: false,
+});
 
 export const AppHeader = () => {
   return (
     <header className="sticky top-0 z-10 h-[60px] flex items-center justify-between px-4 bg-sky-800 shadow-sm shadow-sky-100">
       <h1 className="text-white text-2xl font-bold">{contactInfo.fullName}</h1>
 
-      <PDFDownloadLink
+      <PDFViewer
         document={<PagePdf />}
         fileName={`${contactInfo.fullName} Resume.pdf`}
         className="bg-sky-50 text-sky-800 px-4 py-2 rounded hover:bg-sky-200 transition cursor-pointer font-bold"
       >
         {({ loading, error }) => (error ? 'Failed to load PDF' : loading ? 'Loading document…' : 'Download PDF')}
-      </PDFDownloadLink>
+      </PDFViewer>
     </header>
   )
 }
